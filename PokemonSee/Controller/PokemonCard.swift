9b34048717d42eb580a0f739e2a397cf500bf08d
AnastasiyaDziaruhina <st.vitalievna@yandex.ru>
2022-11-17 23:19:50 +0100

    Added views & controllers

---
+++ PokemonSee/Controller/PokemonCard.swift
@@ -0,0 +1,24 @@
+//
+//  PokemonCard.swift
+//  PokemonSee
+//
+//  Created by Stacy on 17/11/2022.
+//
+
+import Foundation
+import UIKit
+
+class PokemonCard: UIViewController {
+    
+    @IBOutlet weak var pokemonIMG: UIImageView!
+    
+    @IBOutlet weak var pName: UILabel!
+    
+    @IBOutlet weak var pType: UILabel!
+    
+    @IBOutlet weak var pWeight: UILabel!
+    
+    @IBOutlet weak var pHeight: UILabel!
+    
+    
+}
--- PokemonSee/Controller/PokemonsTable.swift
+++ PokemonSee/Controller/PokemonsTable.swift
@@ -5,10 +5,17 @@
 //  Created by Stacy on 17/11/2022.
 //
 
+import Foundation
 import UIKit
 
-class ViewController: UIViewController {
-
+class PokemonsTable: UIViewController {
+    
+    
+    @IBOutlet weak var navigationBar: UINavigationBar!
+    
+    @IBOutlet weak var pokemonsTable: UITableView!
+ 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
